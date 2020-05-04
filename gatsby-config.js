5aa62dea472189c3b05233254b62539f2d91ecd9
Janessa Garrow <janessa.garrow@gmail.com>
2020-05-03 21:52:33 -0600

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: `Blog 'n' Decker`,
+    author: `Janessa Garrow`,
+    description: `Blog posts to accompany slides used for lessons and presentations`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/janessagarrow`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/jgarrow/gatsby-theme-deck-n-blog`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "gatsby-theme-deck-n-blog",
+      options: {
+        contentPath: "decks",
+        blogBasePath: "/posts", // needs to have the forward slash to avoid gatsby Link error message
+        decksBasePath: "/slides", // needs to have forward slash to properly create and update url
+      },
+    },
+  ],
+}
