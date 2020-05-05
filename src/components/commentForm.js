d7be61b89bc49c16fefbd4560df8cee52c07a942
Janessa Garrow <janessa.garrow@gmail.com>
2020-05-04 22:00:06 -0600

    Style form, add validation

--- src/components/commentForm.js
+++ src/components/commentForm.js
@@ -1,8 +1,11 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { jsx, useColorMode } from "theme-ui"
+import { alpha, darken } from "@theme-ui/color"
 import gql from "graphql-tag"
 import { Input, Label, Textarea, Button } from "@theme-ui/components"
+
 import { Formik, Form } from "formik"
+import * as Yup from "yup"
 import { useMutation } from "react-apollo-hooks"
 
 const ADD_COMMENT = gql`
@@ -17,7 +20,39 @@ const ADD_COMMENT = gql`
   }
 `
 
+const CommentSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "**Too Short!")
+    .max(50, "**Too Long!")
+    .required("**Name is required"),
+  comment: Yup.string()
+    .min(1, "**Too Short!")
+    .max(400, "**Comments must be 400 characters or less")
+    .required("**Comment is required"),
+})
+
 const CommentForm = ({ id }) => {
+  const [colorMode] = useColorMode()
+
+  const darkModeForm = {
+    border: (t) => `1px solid ${t.colors.highlight}`,
+
+    ":focus": {
+      bg: (t) => alpha(`${t.colors.primary}`, 0.1),
+    },
+  }
+
+  const darkModeButton = {
+    ":hover": {
+      bg: (t) => darken(`${t.colors.primary}`, 0.05),
+      color: (t) => `${t.colors.text}`,
+    },
+  }
+
+  const darkModeError = { color: (t) => `${t.colors.secondary}` }
+
+  const lightModeError = { color: `#B33A3A` }
+
   const [addComment] = useMutation(ADD_COMMENT)
   const initialValues = {
     name: "",
@@ -37,7 +72,11 @@ const CommentForm = ({ id }) => {
   }
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={CommentSchema}
+      onSubmit={handleSubmit}
+    >
       {({ values, handleChange, errors, touched }) => (
         <Form>
           <Label htmlFor="name">Name</Label>
@@ -48,7 +87,13 @@ const CommentForm = ({ id }) => {
             value={values.name}
             onChange={handleChange}
             mb={3}
+            sx={colorMode === "dark" ? darkModeForm : null}
           />
+          {errors.name && touched.name ? (
+            <p sx={colorMode === "dark" ? darkModeError : lightModeError}>
+              {errors.name}
+            </p>
+          ) : null}
           <Label htmlFor="comment">Comment</Label>
           <Textarea
             id="comment"
@@ -57,18 +102,22 @@ const CommentForm = ({ id }) => {
             onChange={handleChange}
             mb={3}
             rows={5}
+            sx={colorMode === "dark" ? darkModeForm : null}
           />
-          <Button type="submit">Submit</Button>
+          {errors.comment && touched.comment ? (
+            <p sx={colorMode === "dark" ? darkModeError : lightModeError}>
+              {errors.comment}
+            </p>
+          ) : null}
+          <Button
+            type="submit"
+            sx={colorMode === "dark" ? darkModeButton : null}
+          >
+            Submit
+          </Button>
         </Form>
       )}
     </Formik>
-  )
-  return (
-    <div>I might be a form some day</div>
-    // <Box as="form">
-    //   <Label htmlFor="name">Name</Label>
-    //   <Input id="name" />
-    // </Box>
   )
 }
 
