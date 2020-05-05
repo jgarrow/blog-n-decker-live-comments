480682f257b84b0ee4049cdd7f656b6d2a168520
Janessa Garrow <janessa.garrow@gmail.com>
2020-05-04 23:08:11 -0600

    Flesh out theme for comment form

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,4 +1,6 @@
 import theme from "gatsby-theme-deck-n-blog/src/gatsby-plugin-theme-ui"
+import { alpha } from "@theme-ui/color"
+import { deep } from "@theme-ui/presets"
 
 console.log("not shadowing")
 
@@ -6,4 +8,52 @@ console.log(theme)
 
 export default {
   ...theme,
+  colors: {
+    ...theme.colors,
+    modes: {
+      dark: {
+        ...deep.colors,
+        accent: `rgb(102, 153, 204)`,
+      },
+    },
+  },
+  forms: {
+    input: {
+      padding: `5px 10px`,
+      border: (t) => `1px solid ${t.colors.muted}`,
+      transition: `all 0.15s ease-in-out`,
+
+      ":focus": {
+        bg: (t) => alpha(`${t.colors.primary}`, 0.05),
+        outline: `none`,
+        border: (t) => `1px solid ${t.colors.primary}`,
+        boxShadow: (t) => `0px 0px 0px ${t.colors.primary}`,
+      },
+    },
+    textarea: {
+      padding: `5px 10px`,
+      border: (t) => `1px solid ${t.colors.muted}`,
+      transition: `all 0.15s ease-in-out`,
+
+      ":focus": {
+        bg: (t) => alpha(`${t.colors.primary}`, 0.05),
+        outline: `none`,
+        border: (t) => `1px solid ${t.colors.primary}`,
+        boxShadow: (t) => `0px 0px 0px ${t.colors.primary}`,
+      },
+    },
+  },
+  buttons: {
+    primary: {
+      transition: `all 0.15s ease-in-out`,
+      border: `1px solid transparent`,
+
+      ":hover": {
+        cursor: `pointer`,
+        bg: (t) => alpha(`${t.colors.primary}`, 0.05),
+        color: (t) => `${t.colors.primary}`,
+        border: (t) => `1px solid ${t.colors.primary}`,
+      },
+    },
+  },
 }
